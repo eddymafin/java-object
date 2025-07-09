7a63299d970b330cc780643890604eed4a6e6cd7
eddymafin <marumai2451@gmail.com>
2025-07-09 16:28:03 +0900

    クラスフィールド、クラスメソッド追加

--- src/object/Driver.java
+++ src/object/Driver.java
@@ -3,29 +3,37 @@ package object;
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-		SalesEmployee member1 = new SalesEmployee();
-		member1.name = "山田太郎";
-		member1.salesAmt = 200000;
+		//		会社名表示
+		System.out.println();
+		SalesEmployee.displayCompany();
+
+		//		インスタンス数の表示
+		System.out.println();
+		System.out.println("インスタンス数：" + SalesEmployee.instanceCnt);
+
+		SalesEmployee member1 = new SalesEmployee("山田太郎", 200000);
+		//		コンストラクターを設置したので、削除
+		//		member1.name = "山田太郎";
+		//		member1.salesAmt = 200000;
 		System.out.println("");
 		member1.displayInfo();
-		
-		SalesEmployee member2 = new SalesEmployee();
-		member2.name = "鈴木次郎";
-		member2.salesAmt = 500000;
+
+		SalesEmployee member2 = new SalesEmployee("鈴木次郎", 500000);
+		//		コンストラクターを設置したので、削除
+		//		member2.name = "鈴木次郎";
+		//		member2.salesAmt = 500000;
 		System.out.println("");
 		member2.displayInfo();
-		
-//		下記で、計上が逆であることになった
+
+		//		下記で、計上が逆であることになった
 		member1.refund(200000); //20万円返品
-		member2.sell(200000);  //20万円売上
+		member2.sell(200000); //20万円売上
 		System.out.println("---------返品、売上処理後---\n");
 		member1.displayInfo();
 		System.out.println();
 		member2.displayInfo();
 		System.out.println();
-		System.out.println("合計売上金額" +(member1.salesAmt + member2.salesAmt) + "円");
+		System.out.println("合計売上金額" + (member1.salesAmt + member2.salesAmt) + "円");
 
 	}
 
--- src/object/SalesEmployee.java
+++ src/object/SalesEmployee.java
@@ -1,18 +1,47 @@
 package object;
 
-/**
- * 
- */
 public class SalesEmployee {
+
+	/**
+	 * 会社名(static)
+	 */
+	static final String COMPANY_NAME = "株式会社〇〇";
+
+	/**
+	 * 作成したインスタンス数(static) クラス共通の変数
+	 */
+	static int instanceCnt = 0;
+
 	/**
 	 * 名前
 	 */
 	String name;
-	
+
 	/**
 	 * 売上合計金額
 	 */
 	int salesAmt;
+
+	/**
+	 * コンストラクター 
+	 * 名前、売上合計金額の初期化
+	 * @param name 名前初期化
+	 * @param salesAmt 売上合計の初期化
+	 */
+	SalesEmployee(String name, int salesAmt) {
+		this.name = name;
+		this.salesAmt = salesAmt;
+		instanceCnt++; //作成したインスタンス数
+		System.out.println("\nコンストラクタ内");
+		System.out.println("インスタンス数" + instanceCnt);
+	}
+
+	/**
+	 * 会社名の表示 (static)
+	 */
+	static void displayCompany() {
+		System.out.println("会社名：" + COMPANY_NAME);
+	}
 
 	/**
 	 * 売上合計金額
@@ -32,13 +61,12 @@ public class SalesEmployee {
 		return salesAmt -= amt;
 	}
 
-
 	/**
 	 * 情報表示
 	 */
 	void displayInfo() {
-		 System.out.println("名前:" + name);
-		 System.out.println("売上:" + salesAmt + "円");
+		System.out.println("名前:" + name);
+		System.out.println("売上:" + salesAmt + "円");
 	}
 
 }
